6b792441efa3862d788d45054bbea899ed57eb20
bobojonov11 <temurbekbobojonov35@gmail.com>
2025-05-04 12:19:54 +0500

    first js file

--- index.js
+++ index.js
@@ -1,156 +1,185 @@
 //    1-masala
 
-// function logesPalindromicSubstring(s) {
-//     let res = "";
-//     for (let i = 0; i < s.length; i++) {
-//         for (let j = 0; j < s.length; j++) {
-//             let substr = s.slice(i, j + 1);
-//             if (substr === [...substr].reverse().join("") && substr.length > res.length) {
-//                 res = substr;
-//             }
+// function countVowels(str) {
+//     let vowels = "aeiouAEIOU";
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.indexOf(str[i]) !== -1) {
+//             count++;
 //         }
 //     }
-//     return res;
+//     return count;
 // }
-
-// console.log(logesPalindromicSubstring("anajon"));
+// console.log(countVowels("Hello World")); 
 
 
 
 //    2-masala
 
-// function protateArrey(arr, n) {
-//     let sum = arr.splice(0, n);
-//     arr.push(...sum);
-//     return arr
+// function longestCommonPrefix(arr) {
+//     if (!arr.length) return "";
+//     let prefix = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         let j = 0;
+//         while (j < prefix.length && j < arr[i].length && prefix[j] === arr[i][j]) {
+//             j++;
+//         }
+//         prefix = prefix.substring(0, j);
+//     }
+//     return prefix;
 // }
-
-// console.log(protateArrey([1, 2, 3, 4, 5, 6, 7], 3));
+// console.log(longestCommonPrefix(["flower", "flow", "flight"])); 
 
 
 
 //    3-masala
 
-// function twoSum(arr, target) {
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr.length; j++) {
-//             if (arr[i] + arr[j] === target) {
-//                 return [i, j];
-//             }
-//         }
-
+// function minPairSum(arr) {
+//     if (arr.length === 0) return [null, null];
+//     let min = arr[0];
+//     let max = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < min) min = arr[i];
+//         if (arr[i] > max) max = arr[i];
 //     }
+//     return [min, max];
 // }
-
-// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(minPairSum([1, 60, -10, 70, -80, 85]));
 
 
 
 //    4-masala
 
-// function arrSum(arr, target) {
-//     function san(i, total) {
-//         if (total === target) return true;
-//         if (i >= arr.length) return false;
-//         return san(i + 1, total + arr[i])
+// function sumDigitsInString(str) {
+//     let sum = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (!isNaN(str[i]) && str[i] !== ' ') {
+//             sum += parseInt(str[i]);
+//         }
 //     }
-//     return san(0, 0);
+//     return sum;
 // }
-
-// console.log(arrSum([2, 4, 1], 7));
-// console.log(arrSum([2, 4, 2], 7));
+// console.log(sumDigitsInString("a1b2c3")); 
 
 
 
 //    5-masala
 
-// function arrDifect(arr1, arr2) {
-//     const diff1 = arr1.filter(item => !arr2.includes(item));
-//     const diff2 = arr2.filter(item => !arr1.includes(item));
-//     return [...diff1, ...diff2]
+// function allPalindromeSubstrings(str) {
+//     let result = [];
+//     for (let i = 0; i < str.length; i++) {
+//         for (let j = i; j < str.length; j++) {
+//             let substr = str.slice(i, j + 1);
+//             if (isPalindrome(substr)) {
+//                 result.push(substr);
+//             }
+//         }
+//     }
+//     return result;
 // }
 
-// console.log(arrDifect([2, 4, 1], [2, 3, 8]));
+// function isPalindrome(s) {
+//     let left = 0, right = s.length - 1;
+//     while (left < right) {
+//         if (s[left] !== s[right]) return false;
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+
+// console.log(allPalindromeSubstrings("ababa"));
 
 
 
 //    6-masala
 
-// function mostFrequentEliment(arr) {
-//     const freqMap = {};
-//     let maxFreq = 0;
-//     let mostFrequent;
-//     for (let num of arr) {
-//         freqMap[num] = (freqMap[num] || 0) + 1;
-//         if (freqMap[num] > maxFreq) {
-//             maxFreq = freqMap[num];
-//             mostFrequent = num;
+// function findPairs(arr, k) {
+//     let pairs = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === k) {
+//                 pairs.push([i, j]);
+//             }
 //         }
 //     }
-//     return mostFrequent;
+//     return pairs;
 // }
-
-// console.log(mostFrequentEliment([1, 3, 1, 3, 2, 1]));
+// console.log(findPairs([1, 2, 3, 4, 5], 5));
 
 
 
 //    7-masala
 
-// function incrementDigits(str) {
-//       return str.replace(/\d/g, (digit) => {
-//         return (parseInt(digit) + 1) % 10;
-//       });
+// function findPalindromes(start, end) {
+//     let result = [];
+//     for (let i = start; i <= end; i++) {
+//         let str = i.toString();
+//         let isPal = true;
+//         for (let j = 0; j < str.length / 2; j++) {
+//             if (str[j] !== str[str.length - 1 - j]) {
+//                 isPal = false;
+//                 break;
+//             }
+//         }
+//         if (isPal) result.push(i);
 //     }
-
-//     console.log(incrementDigits("abc123"));
+//     return result;
+// }
+// console.log(findPalindromes(100, 150));
 
 
 
 //    8-masala
 
-// function logestIncreasingSubarrey(arr) {
-//   let maxLen = 1, curtLen = 1;
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] > arr[i - 1]) {
-//       curtLen++
-//       maxLen = Math.max(maxLen, curtLen);
-//     } else {
-//       curtLen = 1;
+// function closestFibonacci(n) {
+//     let a = 0, b = 1;
+//     while (b < n) {
+//         let temp = a + b;
+//         a = b;
+//         b = temp;
 //     }
-//   }
-//   return maxLen;
+//     return [a, b];
 // }
-
-// console.log(logestIncreasingSubarrey([10, 20, 30, 10, 40, 50, 52, 55, 6]));
+// console.log(closestFibonacci(10));
 
 
 
 //    9-masala
 
-// function countOccurrences(arr) {
-//   const result = {};
-//   for (let num of arr) {
-//     result[num] = (result[num] || 0) + 1;
-//   }
-//   return result
+// function minMaxDifference(arr) {
+//     let min = arr[0], max = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < min) min = arr[i];
+//         if (arr[i] > max) max = arr[i];
+//     }
+//     return max - min;
 // }
-
-// console.log(countOccurrences([1, 2, 2, 3, 4, 4, 4, 5]));
+// console.log(minMaxDifference([3, 1, 4, 1, 5, 9]));
 
 
 
 //    10-masala
 
-// function matrixSum(matrix) {
-//   let sum = 0;
-//   for (let row of matrix) {
-//     for (let num of row) {
-//       sum += num;
+// function spiralOrder(matrix) {
+//     const res = [];
+//     while (matrix.length) {
+//         res.push(...matrix.shift());
+//         matrix.forEach(row => res.push(row.pop()));
+//         if (matrix.length) res.push(...(matrix.pop().reverse()));
+//         for (let i = matrix.length - 1; i >= 0; i--) {
+//             res.push(matrix[i].shift());
+//         }
 //     }
-//   }
-//   return sum;
+//     return res;
 // }
 
-// console.log(matrixSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+// console.log(spiralOrder(matrix));
 
 
